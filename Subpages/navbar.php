4513a9ec4e81878cd8c3cae59c67231ca9118eac
Oshrestha111 <oriya.shrestha111@gmail.com>
2021-08-16 12:11:19 +0545

    second header by riya

--- Subpages/navbar.php
+++ Subpages/navbar.php
@@ -13,16 +13,21 @@
 
 <!-- Mid Header Layer Starts-->
 <div class="second-header">
-  <div class="second-header-logo">
-    Primeval
-  </div>
-  <div class="search-bar">
-    <input type="text" id="searchbar" class="search-bar" name="searchbar" placeholder="Search....">
-    <div class="btn-search">
-    <img src="./icons/search.png">
-    </div>
-  </div>
-
+  <ul>
+    <li class="logo">Primeval</li>
+    <li class="search-bar">
+      <input type="text" id="searchbar" class="search-bar-input" name="searchbar" placeholder="FIND A SERVICE..">
+      <label class="icon">
+        <i class="fa fa-search" aria-hidden="true"></i>
+      </label>
+    </li>
+    <li class="login">
+      LOG IN 
+    </li>
+    <li>
+      cart 
+    </li>
+  </ul>
 </div>
 
 
@@ -30,13 +35,15 @@
 
 <!-- Lower Header Layer Starts-->
 <nav class="navbar">
+  <ul> 
       <div class="navbar-items">
-        <a href="#">ELECTRONICS</a>
-        <a href="#">WEARS</a>
-        <a href="#">FOODS</a>
-        <a href="#">SHOES</a>
-        <a href="#">MEDICARE</a>
+        <li><a href="#">ELECTRONICS</a></li>
+        <li><a href="#">WEARS</a></li>
+        <li><a href="#">FOODS</a></li>
+        <li><a href="#">SHOES</a></li>
+        <li><a href="#">MEDICARE</a></li>
       </div>
+  </ul>
 </nav>
 
 <!-- Lower Header Layer Ends-->
