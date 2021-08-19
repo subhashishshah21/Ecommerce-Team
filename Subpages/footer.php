40086e1a561a609c34b406e30aa4284676901e3b
Ivey24 <prashish24@gmail.com>
2021-08-19 18:48:23 +0545

    started working on footer

---
+++ Subpages/footer.php
@@ -0,0 +1,26 @@
+
+<div class="ctn-1">
+
+    <div class="newletter">
+        <h3>NEWS LETTER</h3>
+        <input type="text" id="lname" name="lname"><br><br>
+        <button class="btn-signup">SIGN UP</button>
+        <p>Suscribe to our newsletter to get 5% Discount on your next purchase</p>
+    </div>
+    <div class="location">
+        <h3>OUR LOCATION</h3>
+        <br>
+        <a href="`">Sankhamul, New Baneshwor</a>
+        <a href="">+977 -98XXXXXXX / XXXXXXX</a>
+        <a href="">sample@example.com</a>
+    </div>
+    <div class="social">
+        <h3>SOCIAL</h3>
+        <a href=""><img width="20px" height="20px" src="icons/facebook (1).png" alt=""></a>
+        <a href=""><img width="20px" height="20px" src="icons/instagram (1).png" alt=""></a>
+        <a href=""><img width="20px" height="20px" src="icons/youtube1.png" alt=""></a>
+    </div>
+    <div class="payment">
+        <h3>PAYMENT</h3>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -17,7 +17,9 @@
     <?php
 
     include('Subpages/navbar.php');
-    include('Subpages/slider.php')
+    include('Subpages/slider.php');
+
+    include ('Subpages/footer.php');
 ?>
 <!--    js bootstrap-->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
